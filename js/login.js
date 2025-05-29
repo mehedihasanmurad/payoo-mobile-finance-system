3cd090f966bed131207d26ed631a0332485d6a80
Mehedi Hasan Murad <mehedihasna500422@gamil.com>
2025-05-29 11:40:37 +0600

    another page added by event handler

--- js/login.js
+++ js/login.js
@@ -1,7 +1,20 @@
 
 
 document.getElementById("btn-login").addEventListener("click", function (event) {
-    event.preventDefault();
+    // event.preventDefault();//jokhon sob kicu from ar modde thakbe tokhon aita use korbo
+
+    //mobile number and pin number
     const phoneNumber = document.getElementById("phone-number").value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById("pin-number").value;
+
+    //you do not use this system
+    if (phoneNumber === "62" && pinNumber === "1234") {
+        // console.log("Correct phone number and pin number")
+
+        //alada akta file a niye jabo jodi sotto ta sotti hoy
+        window.location.href = "/home.html";
+    }
+    else {
+        alert("Give correct phone and pin number");
+    }
 })
